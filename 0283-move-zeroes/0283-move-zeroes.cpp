7e69ef3d46ed60e9eb318ffbaaa20f8fe6bd43d2
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-04-25 22:16:47 +0530

    Time: 35 ms (14.65%), Space: 19.1 MB (86.56%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int i=0;
+        //for the last non zero position
         for(int j=0;j<nums.size();j++){
             if(nums[j]!=0){
+                //ignoring zero and findin non zero to swap with i
                 swap(nums[i++],nums[j]);
             }
         }
