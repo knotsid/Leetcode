34bc72540e9f99c990209dc4b04b0efa9565155d
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-04-25 22:20:47 +0530

    Time: 31 ms (23.61%), Space: 19.3 MB (17.52%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -5,9 +5,15 @@ public:
         //for the last non zero position
         for(int j=0;j<nums.size();j++){
             if(nums[j]!=0){
-                //ignoring zero and findin non zero to swap with i
-                swap(nums[i++],nums[j]);
+                //ignoring zero and findin non zero copy at i
+                nums[i++] = nums[j];
             }
+        }
+        //since i is pointing at last non zero in nums
+        //and we have copied (not swapped) the non zero at consecutive place
+        //now we can fill the next indices from i with 0
+        for(int j=i;j<nums.size();j++){
+            nums[j]=0;
         }
         
     }
