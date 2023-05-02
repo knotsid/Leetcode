95f38b2747300834d8fd1664858c926a9c7eb5d8
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-05-02 21:25:28 +0530

    Time: 6 ms (23.55%), Space: 8.4 MB (64.42%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,12 +4,16 @@ public:
         int count =0;
         int n = nums.size();
         for(int i=1;i<n;i++){
+//             observation from test cases : previous element is always smaller compares to next element
+//             also considering array as a cyclic array
             if(nums[i-1]>nums[i])
+//                 if observation breaks, increment counter
                 count++;
         }
+//         the cyclic case
         if(nums[n-1]>nums[0])
             count++;
-        
+//         according to observation return false if condition breaks more than 2 times 
         return count<=1;
     }
 };
