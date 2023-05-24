f5f502054f9ff619db7888576262b7b9354b5cd3
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-05-24 22:51:00 +0530

    Time: 108 ms (91.19%), Space: 68.6 MB (36.82%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
+        ios_base::sync_with_stdio(false);
         int temp,mx=-1;
         int n = arr.size();
         for(int i=n-1; i>=0;i--){
