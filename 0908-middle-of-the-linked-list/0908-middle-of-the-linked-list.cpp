c6fe9a0ff607fb16451edcfc4805d97574693ced
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-06-07 23:05:29 +0530

    Time: 0 ms (100%), Space: 7 MB (95.22%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -8,24 +8,53 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//O(n/2)
+//slow and fast pointer method
 class Solution {
 public:
-    int getLen(ListNode* head){
-        int len = 0;
-        while(head!= NULL){
-            len++;
-            head = head->next;
-        }
-        return len;
-    }
-
     ListNode* middleNode(ListNode* head) {
-        int len = getLen(head);
-        int ans = (len/2) ;
-        ListNode* temp = head;
-        while(ans--){
-            temp = temp -> next;
+        if(head ==NULL || head -> next == NULL){
+            return head;
         }
-        return temp;
+
+        //2 nodes
+        if(head -> next -> next == NULL){
+            return head -> next;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head -> next;
+
+        while(fast != NULL){
+            fast = fast -> next;
+            if(fast != NULL){
+                fast = fast -> next;
+            }
+            slow = slow -> next;
+        }
+        return slow;
     }
 };
+//O(n) full array traversal method
+//brute force method
+// class Solution {
+// public:
+//     int getLen(ListNode* head){
+//         int len = 0;
+//         while(head!= NULL){
+//             len++;
+//             head = head->next;
+//         }
+//         return len;
+//     }
+
+//     ListNode* middleNode(ListNode* head) {
+//         int len = getLen(head);
+//         int ans = (len/2) ;
+//         ListNode* temp = head;
+//         while(ans--){
+//             temp = temp -> next;
+//         }
+//         return temp;
+//     }
+// };
