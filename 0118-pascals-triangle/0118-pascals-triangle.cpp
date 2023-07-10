02cb1e5767016d8996733ddd1ffa83fc1e39b65f
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-07-10 23:16:57 +0530

    Time: 5 ms (35.16%), Space: 6.7 MB (74.23%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res(numRows);
-
-        for(int i=0;i<numRows;i++){
-            res[i].resize(i+1);
-            //starts with 1 and ends with 1
-            res[i][0] = res[i][i] = 1;
-
-            //addition logic in middle
-            for(int j=1;j<i;j++){
-                res[i][j] = res[i-1][j-1] + res[i-1][j];
-            }
-        }
-        return res;
-    }
+vector<vector<int>> generate(int numRows) {
+	vector<vector<int>> res;
+	for (int i = 0; i < numRows; i++) {
+		vector<int> row(i + 1, 1);
+		for (int j = 1; j < i; j++) {
+			row[j] = res[i - 1][j] + res[i - 1][j - 1];
+		}
+		res.push_back(row);
+	}
+	return res;
+}
 };
