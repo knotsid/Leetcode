a08bde1701db6d212636002c6f984d63a2fd1389
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-07-13 17:54:52 +0530

    Time: 0 ms (100%), Space: 6.2 MB (98.11%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
+    void solve(string digits, string output, int index, string mapping[], vector<string> &res){
 
-    void solve(string digits, vector<string>& res, string output, int index, string mapping[]){
         //base case
         if(index>=digits.length()){
             res.push_back(output);
             return;
         }
-
-        int number = digits[index] - '0';
-        string val = mapping[number];
-
+        int num = digits[index] - '0';
+        string val = mapping[num];
         for(int i=0;i<val.length();i++){
             output.push_back(val[i]);
-            solve(digits, res, output, index+1, mapping);
+            solve(digits, output, index+1, mapping, res);
             output.pop_back();
         }
-
     }
 
     vector<string> letterCombinations(string digits) {
         vector<string> res;
-        string output;
-        int index=0;
-        string mapping[10] = {
-            "",
-            "",
-            "abc",
-            "def",
-            "ghi",
-            "jkl",
-            "mno",
-            "pqrs",
-            "tuv",
-            "wxyz",
-        };
-        if(digits.length()==0)
+        if(digits.length()==0){
             return res;
-        solve(digits, res, output, index, mapping);
+        }
+        string output = "";
+        int index = 0;
+        string mapping[] = {"", "", "abc","def","ghi","jkl", "mno","pqrs", "tuv","wxyz"};
+        solve(digits, output, index, mapping, res);
         return res;
     }
 };
