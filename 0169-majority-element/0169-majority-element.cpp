c79a239d3975e381ae1ea5a246e0241406ca88cb
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-08-06 12:57:14 +0530

    Time: 14 ms (89.24%), Space: 19.6 MB (53.19%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -6,7 +6,7 @@ public:
         int ele;
         for(int i=0;i<n;i++){
             if(count == 0){
-                count = 1;
+                count =1;
                 ele=nums[i];
             }
             else if(ele == nums[i]){
@@ -16,6 +16,14 @@ public:
                 count--;
             }
         }
-        return ele;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(ele == nums[i]){
+                cnt++;
+            }
+        }
+        if(cnt>n/2)
+            return ele;
+        return -1;
     }
 };
