2dd036265e3b3d275102738062ba027b516ece4f
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-08-07 18:02:53 +0530

    Time: 0 ms (100%), Space: 7 MB (45.6%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,24 +2,15 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        
-        //transposing matrix [j=i] to traverse upper half right of matrix
-        
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                swap(matrix[i][j],matrix[j][i]);
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        
-        //swapping opposite cols
-        
+
         for(int i=0;i<n;i++){
-            int left = 0, right = n-1;
-            while(left<right){
-                swap(matrix[i][left],matrix[i][right]);
-                left++;
-                right--;
-            }
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
+            
