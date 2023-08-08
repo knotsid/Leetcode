b7ced861db55bcf82b26f3037ad21b50c14de398
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-08-08 14:01:26 +0530

    Time: 0 ms (100%), Space: 7 MB (21.63%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,53 +1,41 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        
-        vector<int> res; 
-        //dimensions
-        int row = matrix.size();
-        int col = matrix[0].size();
-        
-        //total elements
-        int totalEle = row * col;
-        int count =0;
-        
-        
-        //indices
-        int startRow = 0;
-        int startCol = 0;
-        int endRow = row - 1;
-        int endCol = col - 1;
-        
-        while(count < totalEle){
-            //print startRow
-            for(int i = startCol;count < totalEle && i<=endCol;i++){
-                res.push_back(matrix[startRow][i]);
-                count++;
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int left = 0, right = m-1;
+        int top = 0, bottom = n-1;
+
+        vector<int> res;
+
+        while(left <= right && top <= bottom){
+
+            //right
+            for(int i=left; i<=right; i++){
+                res.push_back(matrix[top][i]);
             }
-            startRow++;
-            
-            //print endCol
-            for(int i = startRow;count < totalEle && i<=endRow;i++){
-                res.push_back(matrix[i][endCol]);
-                count++;
+            top++;
+
+            for(int i = top; i <= bottom; i++){
+                res.push_back(matrix[i][right]);
             }
-            endCol--;
-            
-            //print endRow
-            for(int i=endCol;count < totalEle && i>=startCol;i--){
-                res.push_back(matrix[endRow][i]);
-                count++;
+            right--;
+
+            if(top<=bottom){
+                for(int i = right; i >= left; i--){
+                res.push_back(matrix[bottom][i]);
+                }
+                bottom--;
             }
-            endRow--;
-            
-            //print startCol
-            for(int i=endRow;count < totalEle && i>=startRow;i--){
-                res.push_back(matrix[i][startCol]);
-                    count++;
+
+            if(left<=right){
+                for(int i = bottom; i >= top; i--){
+                res.push_back(matrix[i][left]);
+                }
+                left++;
             }
-            startCol++;
         }
         return res;
     }
-    
 };
