e090f722e2efae52c0158b5bdb6550f7d3e4def7
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-08-19 16:03:31 +0530

    Time: 0 ms (100%), Space: 8.5 MB (27.5%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,14 +11,41 @@
  */
 class Solution {
 public:
+    // ---------------------RECURSIVE------------------------
+
+    // void solve(TreeNode* root, vector<int> &res){
+    //     if(root == NULL){
+    //         return;
+    //     }
+    //     res.push_back(root->val);
+    //     solve(root->left, res);
+    //     solve(root->right, res);
+    // }
+
+    // ---------------------ITERATIVE------------------------
+
     void solve(TreeNode* root, vector<int> &res){
+
         if(root == NULL){
             return;
         }
-        res.push_back(root->val);
-        solve(root->left, res);
-        solve(root->right, res);
+
+        stack<TreeNode*>st;
+        st.push(root);
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+            res.push_back(root->val);
+            if(root->right != NULL){
+                st.push(root->right);
+            }
+            if(root->left != NULL){
+                st.push(root->left);
+            }
+        }
+        return ;
     }
+
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int>res;
         solve(root, res);
