a0090cf0c6e1725f8159b6c9eb6cc4318f0545bf
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-08-25 22:28:22 +0530

    Time: 10 ms (60.19%), Space: 7.3 MB (48.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length())  //if size is not equal of strings retuen false
-            return false;
-        
-        sort(s.begin(),s.end());    //sort str 1
-        
-        sort(t.begin(),t.end());    //sort str 2
-        
-        if(s==t)                    //is str1 = str2 => they are anagram 
-            return true;
-        return false;
+        unordered_map<char, int>mpp;
+        for(auto i : s){
+            mpp[i]++;
+        }
+        for(auto i : t){
+            mpp[i]--;
+        }
+        for(auto i : mpp){
+            if(i.second!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
