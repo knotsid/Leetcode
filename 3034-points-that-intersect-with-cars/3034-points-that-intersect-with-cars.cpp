a0945f8e6fc68bb223586d72ec19ff9145ce7714
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-09-16 14:02:07 +0530

    Time: 25 ms (27.37%), Space: 27 MB (5.38%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
+    // int numberOfPoints(vector<vector<int>>& nums) {
+    //     sort(nums.begin(),nums.end());
+        
+    //     vector<vector<int>>res;
+        
+    //     int start = nums[0][0];
+    //     int end = nums[0][1];
+        
+    //     for(int i=1;i<nums.size();i++){
+    //         if(nums[i][0]>end){
+    //             res.push_back({start,end});
+    //             start = nums[i][0];
+    //             end = nums[i][1];
+    //         }
+    //         else{
+    //             end = max(end, nums[i][1]);
+    //         }
+    //     }
+    //     res.push_back({start,end});
+    //     int count =0;
+    //     for(int i=0;i<res.size();i++){
+    //         count+=(res[i][1]-res[i][0]+1);
+    //     }
+    //     return count;
+    // }
+
     int numberOfPoints(vector<vector<int>>& nums) {
-        sort(nums.begin(),nums.end());
-        
-        vector<vector<int>>res;
-        
-        int start = nums[0][0];
-        int end = nums[0][1];
-        
-        for(int i=1;i<nums.size();i++){
-            if(nums[i][0]>end){
-                res.push_back({start,end});
-                start = nums[i][0];
-                end = nums[i][1];
-            }
-            else{
-                end = max(end, nums[i][1]);
+        set<int>sett;
+        for(auto ele : nums){
+            int start = ele[0];
+            int end = ele[1];
+            for(int i=start;i<=end;i++){
+                sett.insert(i);
             }
         }
-        res.push_back({start,end});
-        int count =0;
-        for(int i=0;i<res.size();i++){
-            count+=(res[i][1]-res[i][0]+1);
-        }
-        return count;
+        return sett.size();
     }
 };
