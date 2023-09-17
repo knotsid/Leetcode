60f8346316e8a8418a84b84f108782850cf17be3
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-09-17 12:51:56 +0530

    Time: 14 ms (85.66%), Space: 19.8 MB (7.28%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
-        int j=-1;
         int n = arr.size();
+        // finding first 0 in array
+        int j=-1;
         for(int i=0;i<n;i++){
             if(arr[i]==0){
                 j=i;
                 break;
             }
         }
+
+        // edge case : if no zero in array
         if(j==-1){
             return ;
         }
+
+        // swapping of non zero and zero
         for(int i=j+1;i<n;i++){
             if(arr[i]!=0){
                 swap(arr[i],arr[j]);
