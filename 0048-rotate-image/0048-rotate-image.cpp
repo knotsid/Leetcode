a409f5c883dbe3fc024e310fa5e95ca60b5efdc8
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-09-19 13:42:00 +0530

    Time: 0 ms (100%), Space: 7.4 MB (5.96%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,7 +3,7 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
+            for(int j=0;j<i;j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
@@ -13,4 +13,3 @@ public:
         }
     }
 };
-            
