dea338d810ff538174237651f013c630f4739875
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-09-24 15:51:00 +0530

    Time: 3 ms (88.08%), Space: 8.7 MB (8.93%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -13,11 +13,17 @@ public:
     ListNode* reverseList(ListNode* head) {
         ListNode *prev=NULL;
         while(head!=NULL){
+            // assigning next to next node wrt head
             ListNode *next=head->next;
+            // breaking existing pointer to next and putting it on previous node
+            // since no previous of head exist so making it null
             head->next=prev;
+            // cant leave prev behind, we need to move it to keep track of just before head
             prev=head;
+            // updating head to next node and repeating the same process
             head = next;
         }
+        // here head and next are at null so we can return prev or update head and return head
         head=prev;
         return head;
     }
