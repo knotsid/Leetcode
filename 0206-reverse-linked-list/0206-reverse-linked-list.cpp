e8fd0dddc774011eb58832f0b0f18b5190de7263
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-09-24 16:05:26 +0530

    Time: 0 ms (100%), Space: 8.8 MB (5.18%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -12,19 +12,30 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         ListNode *prev=NULL;
-        while(head!=NULL){
-            // assigning next to next node wrt head
-            ListNode *next=head->next;
-            // breaking existing pointer to next and putting it on previous node
-            // since no previous of head exist so making it null
-            head->next=prev;
-            // cant leave prev behind, we need to move it to keep track of just before head
-            prev=head;
-            // updating head to next node and repeating the same process
-            head = next;
-        }
-        // here head and next are at null so we can return prev or update head and return head
-        head=prev;
-        return head;
+        if(head==NULL || head->next==NULL)
+            return head;
+        ListNode *revll=reverseList(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return revll;
     }
+
+    // ____________________ITERATIVE____________________
+    // ListNode* reverseList(ListNode* head) {
+    //     ListNode *prev=NULL;
+    //     while(head!=NULL){
+    //         // assigning next to next node wrt head
+    //         ListNode *next=head->next;
+    //         // breaking existing pointer to next and putting it on previous node
+    //         // since no previous of head exist so making it null
+    //         head->next=prev;
+    //         // cant leave prev behind, we need to move it to keep track of just before head
+    //         prev=head;
+    //         // updating head to next node and repeating the same process
+    //         head = next;
+    //     }
+    //     // here head and next are at null so we can return prev or update head and return head
+    //     head=prev;
+    //     return head;
+    // }
 };
