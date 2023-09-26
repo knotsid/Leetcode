27bfbff67bb3c1663449713ae468f43ee6bbbb2c
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-09-26 14:05:43 +0530

    Time: 5 ms (24.89%), Space: 7 MB (17.7%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char ch=0;
-        for(char itr:s) ch^=itr;
-        for(char itr:t) ch^=itr;
-        return ch;
+        // char ch=0;
+        // for(char itr:s) ch^=itr;
+        // for(char itr:t) ch^=itr;
+        // return ch;
+
+        for(int i=0;i<s.size();i++){
+            t[i+1]+=t[i]-s[i];
+        }
+        return t[t.size()-1];
     }
 };
