4009c53d92490c7cc46a02c31e4f1d1c165df05b
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-10-06 18:58:16 +0530

    Time: 0 ms (100%), Space: 9 MB (13.58%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,15 +11,16 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int> &res){
-        if(root){
-            solve(root->left,res);
-            solve(root->right, res);
-            res.push_back(root->val);
-        }
+    void solve(TreeNode *root, vector<int> &res){
+        if(root==NULL) return;
+
+        solve(root->left,res);
+        solve(root->right,res);
+        res.push_back(root->val);
     }
+
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int>res;
+        vector<int> res;
         solve(root, res);
         return res;
     }
