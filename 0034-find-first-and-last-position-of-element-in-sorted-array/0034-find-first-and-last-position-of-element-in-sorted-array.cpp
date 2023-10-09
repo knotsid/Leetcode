0134cc3c2103b428a4bb65be9ce9dec9a2902448
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-10-09 23:57:22 +0530

    Time: 7 ms (51.15%), Space: 14.6 MB (8.57%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,47 +1,25 @@
 class Solution {
 public:
-    int first_occ(vector<int>nums, int target){
-        int n=nums.size();
-        int st=0, end=n-1;
-        int res=-1;
+    int lower_bound(vector<int> nums, int st, int end, int target){
         while(st<=end){
-            int mid = st+(end-st)/2;
-            if(nums[mid]==target){
-                res=mid;
-                end=mid-1;
-            }
-            else if(nums[mid]<target){
+            int mid = st + (end-st)/2;
+            if(nums[mid]<target){
                 st=mid+1;
             }
             else{
-                end =mid-1;
+                end = mid-1;
             }
         }
-        return res;
-    }
-    int last_occ(vector<int>nums, int target){
-        int n=nums.size();
-        int st=0, end=n-1;
-        int res=-1;
-        while(st<=end){
-            int mid = st+(end-st)/2;
-            if(nums[mid]==target){
-                res=mid;
-                st=mid+1;
-            }
-            else if(nums[mid]<target){
-                st=mid+1;
-            }
-            else{
-                end =mid-1;
-            }
-        }
-        return res;
+        return st;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> v;
-        v.push_back(first_occ(nums, target));
-        v.push_back(last_occ(nums, target));
-        return v;
+        int st=0, end = nums.size()-1;
+        int leftPos = lower_bound(nums, st, end, target);
+        int rightPos = lower_bound(nums, st, end, target+1)-1;
+
+        if(leftPos<nums.size() && nums[leftPos]==target){
+            return {leftPos, rightPos};
+        }
+        return {-1,-1};
     }
 };
