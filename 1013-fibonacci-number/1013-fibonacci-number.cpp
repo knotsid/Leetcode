4dcca483773505fa2e18539c9c5c18d1bf343e1b
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-10-15 21:57:23 +0530

    Time: 2 ms (78.01%), Space: 6.3 MB (36.4%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+    int fib(int N) {
+        if(N < 2) 
+            return N;
+    	int a = 0, b = 1, c = 0;
+        for(int i = 1; i < N; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
     }
 };
