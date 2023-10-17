58c007a1cc40e74d111e14293717f77ee84839c7
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-10-17 23:45:52 +0530

    Time: 79 ms (97.2%), Space: 57.6 MB (76.62%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> mpp;
-        for(auto i : nums){
-            mpp[i]++;
-        }
-        for(auto i:mpp){
-            if(i.second>1){
+        //_____________HASHMAP_____________
+        // unordered_map<int,int> mpp;
+        // for(auto i : nums){
+        //     mpp[i]++;
+        // }
+        // for(auto i:mpp){
+        //     if(i.second>1){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        //_____________SORT_____________
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[i] == nums[i-1]){
                 return true;
             }
         }
