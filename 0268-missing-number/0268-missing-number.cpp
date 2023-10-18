ed6864336aaee9bcd240bbbd87a9c693b1d039c5
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-10-18 19:38:05 +0530

    Time: 15 ms (59.59%), Space: 18.5 MB (7.27%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int res = nums.size();
-        for(int i=0; i<nums.size(); i++){
-            res ^= i;
-            res ^= nums[i];
+        int res=nums.size();
+        for(int i=0;i<nums.size();i++){
+            res^=i;
+            res^=nums[i];
         }
         return res;
     }
