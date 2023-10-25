361d7d16ab661f0ed6fa57363992fb576c539965
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-10-25 18:28:44 +0530

    Time: 2 ms (50.08%), Space: 6.2 MB (66.1%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,19 +1,39 @@
-class Solution {
-public:
-    int mySqrt(int x) {
-        if(x==0) return x;
-        int st=1, end =x;
-        while(st<=end){
-            int mid = st + (end - st)/2;
-            if(mid==x/mid) 
-                return mid;
-            else if(mid>x/mid){
-                end = mid-1;
+class Solution
+{
+    public:
+       	// int mySqrt(int x) {
+       	//     if(x==0) return x;
+       	//     int st=1, end =x;
+       	//     while(st<=end){
+       	//         int mid = st + (end - st)/2;
+       	//         if(mid==x/mid) 
+       	//             return mid;
+       	//         else if(mid>x/mid){
+       	//             end = mid-1;
+       	//         }
+       	//         else{
+       	//             st=mid+1;
+       	//         }
+       	//     }
+       	//     return end;
+       	// }
+        int mySqrt(int x)
+        {
+            if (x == 0) return x;
+            int st = 1, end = x;
+            while (st <= end)
+            {
+                int mid = st + (end - st) / 2;
+                if (mid == x / mid) return mid;
+                else if (mid > x / mid)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    st = mid + 1;
+                }
             }
-            else{
-                st=mid+1;
-            }
+            return end;
         }
-        return end;
-    }
 };
