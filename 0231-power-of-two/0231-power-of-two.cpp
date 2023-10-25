af64f3920bb2fe03caa79ddf4ed99a3c4c898412
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-10-25 14:39:46 +0530

    Time: 0 ms (100%), Space: 6.2 MB (51.2%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<1) return 0;
-        while(n!=1){
-            if(n%2==1) return 0;
-            n/=2;
-        }
-        return 1;
+        // if(n<1) return false;
+        // while(n!=1){
+        //     if(n%2==1) return false;
+        //     n/=2;
+        // }
+        // return true;
+
+        if(n<=0) return false;
+        return (n&(n-1))==0;
     }
 };
