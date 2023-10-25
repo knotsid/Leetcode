571ad397a797ab410b70baa14df2fb0a48bc2be1
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-10-25 19:42:41 +0530

    Time: 2 ms (47.54%), Space: 6.4 MB (10.74%) - LeetHub

---
+++ 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int mask = 1;
+        while((mask&n)!=n){
+            mask = mask<<1;
+            mask+=1;
+        }
+        return mask ^ n;
+    }
+};
