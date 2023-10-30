27e02409c1ac11902690d2d725df2883a79ccc9f
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-10-30 16:43:48 +0530

    Time: 45 ms (98.13%), Space: 46.3 MB (15.45%) - LeetHub

--- 0792-binary-search/0792-binary-search.ts
+++ 0792-binary-search/0792-binary-search.ts
@@ -1,10 +1,21 @@
+function solve(nums: number[], target: number, st:number, end: number):number{
+    if(st>end) return -1;
+
+    let mid = Math.floor((st+end)/2);
+    if(nums[mid]==target)  return mid;
+    else if(nums[mid]<target) return solve(nums, target, mid+1, end);
+    else return solve(nums, target, st, mid-1);
+}
+
 function search(nums: number[], target: number): number {
     let st = 0, end = nums.length - 1;
-    while (st <= end) {
-        let mid = Math.floor((st+end)/2);
-        if(nums[mid]==target) return mid;
-        else if(nums[mid]<target) st=mid+1;
-        else end = mid -1;
-    }
-    return -1;
+    // while (st <= end) {
+    //     let mid = Math.floor((st+end)/2);
+    //     if(nums[mid]==target) return mid;
+    //     else if(nums[mid]<target) st=mid+1;
+    //     else end = mid -1;
+    // }
+    // return -1;
+    let ans = solve(nums, target, st, end)
+    return ans;
 }
