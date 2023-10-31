6bce7dfb79ca3aade504d7f1abecae589873c0ad
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-10-31 19:12:05 +0530

    Time: 0 ms (100%), Space: 11.4 MB (26.23%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
-    int find_pivot(vector<int>nums){
-        int n=nums.size();
-        int st=0, end = n-1;
-        while(st<end){
-            int mid = st+(end-st)/2;
-            if(nums[0]<=nums[mid]){
-                st=mid+1;
-            }
-            else{
-                end = mid;
-            }
-        }
-        return st;
-    }
-    int binary_search(vector<int>nums, int st, int end, int target){
-        while(st<=end){
-            int mid = st+(end-st)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(target>nums[mid]){
-                st=mid+1;
-            }
-            else{
-                end = mid-1;
-            }
-        }
-        return -1;
-    }
     int search(vector<int>& nums, int target) {
-        int pivot = find_pivot(nums);
-        if(target>=nums[pivot]&&target<=nums[nums.size()-1]){
-            return binary_search(nums, pivot, nums.size()-1, target);
-        }
-        else{
-            return binary_search(nums, 0, pivot, target);
+        int n = nums.size();
+        int st=0, end =n-1;
+
+        while(st<=end){
+            int mid = st + (end - st)/2;
+            if(nums[mid]==target) return mid;
+
+            //left sorted half
+            if(nums[st]<=nums[mid]){
+                if(target <= nums[mid] && target >= nums[st]) end = mid-1;
+                else st = mid+1;
+            }
+
+            //right sorted half
+            else{
+                if(target >= nums[mid] && target <=nums[end]) st=mid+1;
+                else end = mid-1;
+            }
         }
         return -1;
     }
