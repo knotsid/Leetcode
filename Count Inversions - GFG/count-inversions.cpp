6782bef236f7e1365049d8f8dda408f8a8f9f44e
Not Sid <36338447+knotsid@users.noreply.github.com>
2023-11-22 23:43:05 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -15,20 +15,20 @@ class Solution{
         mergeSort(arr, 0, N-1);
         return ans;
     }
-    void mergeSort(ll arr[], ll lo, ll hi) {
-        if(lo<hi) {
-            ll mid = lo + (hi-lo)/2;
-            mergeSort(arr, lo, mid);
-            mergeSort(arr, mid+1, hi);
-            merge(arr, lo, mid, hi);
+    void mergeSort(ll arr[], ll st, ll end) {
+        if(st<end) {
+            ll mid = st + (end-st)/2;
+            mergeSort(arr, st, mid);
+            mergeSort(arr, mid+1, end);
+            merge(arr, st, mid, end);
         }
     }
-    void merge(ll arr[], ll lo, ll mid, ll hi) {
-        ll n1 = mid-lo+1, n2 = hi-mid;
+    void merge(ll arr[], ll st, ll mid, ll end) {
+        ll n1 = mid-st+1, n2 = end-mid;
         ll arr1[n1], arr2[n2];
-        for(ll i=0; i<n1; i++) arr1[i] = arr[lo+i];
+        for(ll i=0; i<n1; i++) arr1[i] = arr[st+i];
         for(ll i=0; i<n2; i++) arr2[i] = arr[mid+i+1];
-        ll i=0, j=0, k=lo;
+        ll i=0, j=0, k=st;
         while(i<n1 && j<n2) {
             if(arr1[i]<=arr2[j]) arr[k++] = arr1[i++];
             else {
