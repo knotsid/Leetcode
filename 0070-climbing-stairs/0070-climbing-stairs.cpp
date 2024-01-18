ddb98ca6fe38623d3ce322dc7457867b66354e18
Not Sid <36338447+knotsid@users.noreply.github.com>
2024-01-18 22:29:36 +0530

    Time: 3 ms (16.28%), Space: 6.7 MB (18.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-    int climber(vector<int> &dp,int n){
-        if(n<0) return 0;
-        if(n==0 || n==1)    return 1;
-        if(dp[n]!=-1)   return dp[n];
-        return dp[n]=climber(dp,n-1)+climber(dp,n-2);
+    int solve(int n,vector<int> &dp){
+        if(n<=2)
+          return n;
+        
+        if(dp[n]!=-1) 
+          return dp[n]; 
+        
+        dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        return dp[n];
     }
     int climbStairs(int n) {
+        if(n<=2)
+         return n;
         vector<int> dp(n+1,-1);
-        return climber(dp,n);
+        return solve(n,dp);
     }
 };
